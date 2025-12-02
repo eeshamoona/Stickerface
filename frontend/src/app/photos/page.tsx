8339ed05c4696a59e770d48d120da5a6ce1810ff
eeshamoona <eeshamoona@gmail.com>
2025-12-02 17:04:52 -0600

    fix: remove extraneous spaces from photo link href

--- frontend/src/app/photos/page.tsx
+++ frontend/src/app/photos/page.tsx
@@ -61,7 +61,7 @@ export default function PhotosPage() {
             {photos.map((photo) => (
               <Link
                 key={photo.id}
-                href={`/ photos / ${photo.slug || photo.id} `}
+                href={`/photos/${photo.slug || photo.id}`}
                 className="group block break-inside-avoid mb-6"
               >
                 <article className="flex flex-col gap-3">
